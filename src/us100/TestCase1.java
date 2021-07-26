923f701043f0f3fa95ac103c30bec7bf5dc3dc2f
Feruza Jonzokova <feruzka1706@gmail.com>
2021-07-26 13:16:39 -0700

    finished the whole story 100

--- src/us100/TestCase1.java
+++ src/us100/TestCase1.java
@@ -4,5 +4,6 @@ public class TestCase1 {
     public static void main(String[] args) {
         System.out.println("Github class");
         //awesome code goes here part 1
+        // awesome code finished up here
     }
 }
