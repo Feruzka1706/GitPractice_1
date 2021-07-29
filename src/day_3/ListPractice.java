22e2e06c8ea50e991d7d54c9e032b6835e497642
Feruza Jonzokova <feruzka1706@gmail.com>
2021-07-29 11:33:51 -0700

    created new ArrayList

--- src/day_3/ListPractice.java
+++ src/day_3/ListPractice.java
@@ -1,8 +1,12 @@
 package day_3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ListPractice {
     public static void main(String[] args) {
         //we are still practicing github stuff
-        //
+        ArrayList<String> students=new ArrayList<>();
+        students.addAll(Arrays.asList("Feruza","Nazli","Gulistan"));
     }
 }
