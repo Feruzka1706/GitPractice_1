1f0b7bd63678bbd25d9fe88e9de8b86f92f6bce4
Feruza Jonzokova <feruzka1706@gmail.com>
2021-07-29 11:46:08 -0700

    set is one of the Collection utility method

--- src/day_3/SelfPractice.java
+++ src/day_3/SelfPractice.java
@@ -3,4 +3,5 @@ package day_3;
 public class SelfPractice {
     //we are making commit
     //Set is a collection type that only store unique item
+    //if you add duplicate it will just remove it
 }
