eb6cd9fc4a7fc7023bd7b710cc143047c3830117
Feruza Jonzokova <feruzka1706@gmail.com>
2021-07-29 14:34:03 -0700

    some cars we added

--- src/day_3/Car.java
+++ src/day_3/Car.java
@@ -1,10 +1,13 @@
 package day_3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Car {
     public static void main(String[] args) {
         ArrayList<String> cars=new ArrayList<>();
         //trying to add some commits
+
+        cars.addAll(Arrays.asList("Tesla","Honda"));
     }
 }
