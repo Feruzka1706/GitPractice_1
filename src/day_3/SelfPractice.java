7f9f716e4bf42035f986c20141ced886c291cde8
Feruza Jonzokova <feruzka1706@gmail.com>
2021-07-29 11:46:46 -0700

    we are learning set method soon

--- src/day_3/SelfPractice.java
+++ src/day_3/SelfPractice.java
@@ -4,4 +4,5 @@ public class SelfPractice {
     //we are making commit
     //Set is a collection type that only store unique item
     //if you add duplicate it will just remove it
+    //it's a collection of unique elements
 }
