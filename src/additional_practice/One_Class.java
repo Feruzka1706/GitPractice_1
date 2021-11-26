c9abfd04c724f57b00d112040668cd77c23ed762
Feruza Jonzokova <feruzka1706@gmail.com>
2021-11-26 00:43:25 -0800

    Updated One_Class

--- src/additional_practice/One_Class.java
+++ src/additional_practice/One_Class.java
@@ -3,4 +3,11 @@ package additional_practice;
 public class One_Class {
     //first commit here
 
+    //From new branch is coming those comments
+    /**
+     * Local Development
+     *
+     * 1. Open new file
+     */
+
 }
